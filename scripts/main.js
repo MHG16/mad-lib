122d426a42152affcb2c5c55659532723962f94b
Martin Grossmann <mhgrossmann@yahoo.com>
2016-03-07 20:32:58 -0600

    Explorer mode complete

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,10 @@
-var noun = prompt('Enter a noun');
-var verb = prompt('Enter a verb');
-var adjective = prompt('Enter an adjective');
-var adverb = prompt('Enter an adverb');
+var noun = prompt('Enter a noun:');
+var verb = prompt('Enter a verb:');
+var adjective = prompt('Enter an adjective:');
+var adverb = prompt('Enter an adverb:');
 
 
 //The adj alien landed on the planet adverb.  He then verb his raygun at the noun.
 console.log('The '+adjective+ ' alien landed on the planet '+adverb+ '.  It then '+ verb + ' its ray gun at the '+ noun + '.');
-
-
 
 
